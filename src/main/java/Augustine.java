26542d4f63d73b6477c2934e21dedb129b3013dc
kevinlokewy <kevinlokeyy@gmail.com>
2025-09-01 15:34:47 +0800

    add echo

--- src/main/java/Augustine.java
+++ src/main/java/Augustine.java
@@ -1,11 +1,29 @@
+import java.util.Scanner;
+
 public class Augustine {
     public static void main(String[] args) {
+        Scanner line = new Scanner (System.in);
         System.out.println("____________________________________________________________");
         System.out.println(" Hello! I'm Augustine");
         System.out.println(" What can I do for you?");
         System.out.println("____________________________________________________________");
-        System.out.println(" Bye. Hope to see you again soon!");
-        System.out.println("____________________________________________________________");
+
+        while (true){
+            String input = line.nextLine();
+
+            if (input.equalsIgnoreCase("bye")){
+                System.out.println("____________________________________________________________");
+                System.out.println(" Bye. Hope to see you again soon!");
+                System.out.println("____________________________________________________________");
+                break;
+            }
+
+            System.out.println("____________________________________________________________");
+            System.out.println(" " + input);
+            System.out.println("____________________________________________________________");
+        }
+
+        line.close();
     }
 }
 
