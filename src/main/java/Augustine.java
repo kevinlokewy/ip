68af15b6a9d72a6181bcfe45a6196a7f4b7a8e92
kevinlokewy <kevinlokeyy@gmail.com>
2025-09-01 20:21:04 +0800

    Add list

--- src/main/java/Augustine.java
+++ src/main/java/Augustine.java
@@ -2,56 +2,28 @@ import java.util.Scanner;
 
 public class Augustine {
     public static void main(String[] args) {
-        Scanner line = new Scanner(System.in);
-        String[] tasks = new String[100];
-        int taskCount = 0;
+        Scanner line = new Scanner (System.in);
         System.out.println("____________________________________________________________");
         System.out.println(" Hello! I'm Augustine");
         System.out.println(" What can I do for you?");
         System.out.println("____________________________________________________________");
 
-        while (true) {
-            String input = line.nextLine().trim();
+        while (true){
+            String input = line.nextLine();
 
-            if (input.toLowerCase().contains("bye")) {
+            if (input.equalsIgnoreCase("bye")){
                 System.out.println("____________________________________________________________");
                 System.out.println(" Bye. Hope to see you again soon!");
                 System.out.println("____________________________________________________________");
                 break;
-            } else if (input.equalsIgnoreCase("list")) {
-                System.out.println("____________________________________________________________");
-                if (taskCount == 0) {
-                    System.out.println("No tasks added yet!");
-                } else {
-                    for (int i = 0; i < taskCount; i += 1) {
-                        System.out.println(" " + (i + 1) + ". " + tasks[i]);
-                    }
-                    System.out.println("____________________________________________________________");
-                }
-
-            } else if (input.toLowerCase().startsWith("add")) {
-                if (taskCount < 100) {
-                    String task = input.substring(4).trim(); // remove "add " prefix
-                    tasks[taskCount] = input;
-                    taskCount += 1;
-                    System.out.println("____________________________________________________________");
-                    System.out.println(" added:" + input);
-                    System.out.println("____________________________________________________________");
-                } else {
-                    System.out.println("____________________________________________________________");
-                    System.out.println("Task list is full! Cannot add anymore tasks");
-                    System.out.println("____________________________________________________________");
-                }
-            } else {
-
-                System.out.println("____________________________________________________________");
-                System.out.println(" " + input);
-                System.out.println("____________________________________________________________");
-
             }
-        }
-        line.close();
 
+            System.out.println("____________________________________________________________");
+            System.out.println(" " + input);
+            System.out.println("____________________________________________________________");
+        }
+
+        line.close();
     }
 }
 
